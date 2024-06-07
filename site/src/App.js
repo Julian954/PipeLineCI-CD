a0b5b1b1365883a510c37b0df506f708eda6e00c
Julian Sanchez <82236836+Julian954@users.noreply.github.com>
2024-06-07 17:44:57 -0600

    Update App.js
    actualizacion de error de component

--- site/src/App.js
+++ site/src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Auth from './pages/Auth/Auth'
-import Dashboard from './pages/Dashboard/Dashboard'
-import { getSession } from './utils'
+} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Auth from './pages/Auth/Auth';
+import Dashboard from './pages/Dashboard/Dashboard';
+import { getSession } from './utils';
 
 export default class App extends Component {
+  static propTypes = {
+    // Define las propTypes que esperas recibir como props
+  };
 
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   async componentDidMount() {
@@ -24,24 +28,20 @@ export default class App extends Component {
     return (
       <Router>
         <Switch>
-
           <Route path='/register'>
             <Auth />
           </Route>
-
           <Route path='/login'>
             <Auth />
           </Route>
-
           <PrivateRoute
             exact
             path='/'
             component={Dashboard}
           />
-
         </Switch>
       </Router>
-    )
+    );
   }
 }
 
@@ -49,10 +49,17 @@ export default class App extends Component {
  * A component to protect routes.
  * Shows Auth page if the user is not authenticated
  */
-const PrivateRoute = ({ component, ...options }) => {
+const PrivateRoute = ({ component: Component, ...options }) => {
+  return (
+    <Route
+      {...options}
+      render={(props) =>
+        getSession() ? <Dashboard {...props} /> : <Home {...props} />
+      }
+    />
+  );
+};
 
-  const session = getSession()
-
-  const finalComponent = session ? Dashboard : Home
-  return <Route {...options} component={finalComponent} />
-}
+PrivateRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+};
